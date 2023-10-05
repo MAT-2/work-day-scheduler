40c8ac43a5489a92dc03cde2ddf84b744269d83c
MAT-2 <mpcasey30@gmail.com>
2023-10-05 18:20:20 -0500

    Added for each loop for buttons variable to capture an event listener for each button clicked. Used console.log to test, and it works! Added update for references used.

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,14 @@ var time = dayjs();
 var hour = time.format("h:mmA");
 console.log(hour);
 
+var buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("Button Clicked!");
+  });
+});
+
 $("#currentDay").text(time.format("dddd, MMM D, YYYY [at] h:mmA"));
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
@@ -14,7 +22,6 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # work-day-scheduler
+
 Module 5 - A project containing a fully dynamic work scheduler.
+
+## References
+
+- https://www.queryselectorall.com/buttons
+- https://medium.com/@ipshubham/the-ultimate-guide-to-understanding-local-storage-2a27fdb80544
+- https://day.js.org/docs/en/display/format
+- https://www.w3schools.com/js/js_arrow_function.asp
+- https://www.w3schools.com/jsref/jsref_foreach.asp
