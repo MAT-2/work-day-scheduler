34229ecc551e5549ee7b61bdd67fa88738c6453d
MAT-2 <mpcasey30@gmail.com>
2023-10-05 16:31:44 -0500

    Added more divs to show the 9-5 schedule. Fixed issue using correct variable name when using the DAYJS method for displaying the day in the header.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,7 @@
+var time = dayjs();
+
+$("#currentDay").text(time.format("dddd, MMM D, YYYY"));
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
